2b5c92ad3a1a23d1aefacc9dbd40497ace4ee9b4
john-craig <john.e.craig.jr@gmail.com>
2024-10-30 16:40:31 -0400

    laptop: Add laptop

--- flake.nix
+++ flake.nix
@@ -26,6 +26,16 @@
                 ./users/evak/home.nix 
               ];
           };
+          "evak@laptop" = home-manager.lib.homeManagerConfiguration {
+              # Note: I am sure this could be done better with flake-utils or something
+              pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+              modules = [ 
+                { nixpkgs.overlays = [ nixpkgs-apocrypha.overlays.default ]; }
+                ./hosts/laptop
+                ./users/evak/home.nix 
+              ];
+          };
       };
   };
 }
--- users/evak/home.nix
+++ users/evak/home.nix
@@ -24,8 +24,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    faustroll
-    panmuphle
+    # faustroll
+    # panmuphle
   ];
 
 
