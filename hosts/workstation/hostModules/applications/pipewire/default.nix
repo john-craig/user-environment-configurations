a7f67c501d4d307b1f404b18783da1dff3672d46
john-craig <john.e.craig.jr@gmail.com>
2024-10-30 20:51:05 -0400

    workstation: Remove echo cancellation for pipewire

--- hosts/workstation/hostModules/applications/pipewire/default.nix
+++ hosts/workstation/hostModules/applications/pipewire/default.nix
@@ -2,6 +2,6 @@
 {
   home.file = {
     ".config/pipewire/pipewire.conf.d/loopback-instrument.conf".source = ./loopback-instrument.conf;
-    ".config/pipewire/pipewire.conf.d/echo-cancellation.conf".source = ./echo-cancellation.conf;
+    # ".config/pipewire/pipewire.conf.d/echo-cancellation.conf".source = ./echo-cancellation.conf;
   };
 }
