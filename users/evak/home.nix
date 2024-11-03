557748a30a29058e090f21e18cf85d5889509641
john-craig <john.e.craig.jr@gmail.com>
2024-11-03 17:56:02 -0500

    shell: Fix issue with PATH containing erroneous stuff

--- users/evak/home.nix
+++ users/evak/home.nix
@@ -25,11 +25,10 @@
   # environment.
   home.packages = with pkgs; [
     dismas
-    # faustroll
+    faustroll
     panmuphle
   ];
-
-
+  
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
--- users/evak/userModules/shell/path/default.nix
+++ users/evak/userModules/shell/path/default.nix
@@ -1,12 +1,17 @@
 { config, pkgs, lib, ... }:
-{
-  home.sessionVariables = let 
+let 
     paths = [
       "$HOME/.nix-profile/bin"
       "$HOME/.pyenv/bin"
       "/opt/android-sdk/cmdline-tools/latest/bin"
     ];
-  in  {
-    PATH = builtins.concatStringsSep ":" (["$PATH"] ++ paths);
+in {
+  home.sessionVariables = {
+    PATH = builtins.concatStringsSep ":" (paths ++ ["$PATH"]);
   };
+
+  # Scrape all that nonsense off the front of our PATH
+  programs.zsh.initExtra = ''
+    PATH="${builtins.elemAt paths 0}''${PATH#*${builtins.elemAt paths 0}}"
+  '';
 }
