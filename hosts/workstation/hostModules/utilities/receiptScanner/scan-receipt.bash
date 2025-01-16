239bf7296ae868e97c2dab0b9e45a28f34251cf9
john-craig <john.e.craig.jr@gmail.com>
2025-01-16 06:52:23 -0500

    Tweak scanner script

--- hosts/workstation/hostModules/utilities/receiptScanner/scan-receipt.bash
+++ hosts/workstation/hostModules/utilities/receiptScanner/scan-receipt.bash
@@ -7,15 +7,16 @@ scanimage --format=jpeg --output-file $DOC_PATH
 
 # Define the file path and API token
 API_TOKEN_PATH="/sec/paperless/workstation/evak/api-token.txt"
-API_TOKEN=$(cat $API_TOKEN_PATH)
+# API_TOKEN=$(cat $API_TOKEN_PATH)
 
 # Define the endpoint URL
 URL="https://paperless.chiliahedron.wtf/api/documents/post_document/"
 
 # Perform the POST request using curl with Authorization header
+{ echo -n 'Authorization: Token '; cat $API_TOKEN_PATH | tr -d '\n'; } | \
 curl --silent -X POST "$URL" \
   -F "document=@$DOC_PATH" \
-  -H "Authorization: Token $API_TOKEN" \
+  -H @- \
   -H "Content-Type: multipart/form-data"
 
 rm $DOC_PATH
