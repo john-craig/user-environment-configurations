98d3a242ffc555074a64d82bf0688c50f2b888af
john-craig <john.e.craig.jr@gmail.com>
2025-05-09 12:20:09 -0400

    Commit work on hyprland

--- users/evak/userModules/applications/hypr/default.nix
+++ users/evak/userModules/applications/hypr/default.nix
@@ -1,7 +1,27 @@
 { config, pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    # hyprland
+    # hypridle
+    # hyprpaper
+    # hyprlock
+    # hyprcursor
+
+    nixgl.auto.nixGLDefault
+    nixgl.auto.nixGLNvidia
+  ];
+
+  # home.sessionVariables = {
+  #   LIBGL_DRIVERS_PATH = "/usr/lib/dri";
+  #   GBM_BACKENDS_PATH = "/usr/lib/gbm";
+  # };
+
+  # wayland.windowManager.hyprland.enable = true; # enable Hyprland
+
   home.file = {
     ".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
     ".config/hypr/hyprland.conf".source = ./hyprland.conf;
+    # ".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
+    # ".config/hypr/hypridle.conf".source = ./hypridle.conf;
   };
 }
