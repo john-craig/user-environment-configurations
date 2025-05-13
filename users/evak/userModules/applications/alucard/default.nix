10ae44060ff4ecafbd15a8852f4f2bbf33f0b851
john-craig <john.e.craig.jr@gmail.com>
2025-05-12 22:56:37 -0400

    Merge feature branch 'update-dismas' into main

--- users/evak/userModules/applications/alucard/default.nix
+++ users/evak/userModules/applications/alucard/default.nix
@@ -10,7 +10,7 @@
   };
 
   programs.zsh.shellAliases = {
-    "deploy-host" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $HOMELAB_CONFIGURATIONS) alucard deploy host --flake $HOMELAB_CONFIGURATIONS --target remote --type development";
-    "deploy-user" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $USER_ENVIRONMENT_CONFIGURATIONS) alucard deploy user --flake $USER_ENVIRONMENT_CONFIGURATIONS --type development";
+    "deploy-host" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $HOMELAB_CONFIGURATIONS) alucard deploy host --flake $HOMELAB_CONFIGURATIONS --target remote --type development";
+    "deploy-user" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $USER_ENVIRONMENT_CONFIGURATIONS) alucard deploy user --flake $USER_ENVIRONMENT_CONFIGURATIONS --type development";
   };
 }
--- users/evak/userModules/shell/aliases/default.nix
+++ users/evak/userModules/shell/aliases/default.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 {
   programs.zsh.shellAliases = {
-    "deploy-host" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $HOMELAB_CONFIGURATIONS) alucard deploy host --flake $HOMELAB_CONFIGURATIONS --target remote --type development";
-    "deploy-user" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $USER_ENVIRONMENT_CONFIGURATIONS) alucard deploy user --flake $USER_ENVIRONMENT_CONFIGURATIONS --type development";
-    "deploy-cluster" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $HOMELAB_CONFIGURATIONS) alucard deploy cluster --flake $HOMELAB_CONFIGURATIONS --type development";
+    "deploy-host" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $HOMELAB_CONFIGURATIONS) alucard deploy host --flake $HOMELAB_CONFIGURATIONS --target remote --type development";
+    "deploy-user" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $USER_ENVIRONMENT_CONFIGURATIONS) alucard deploy user --flake $USER_ENVIRONMENT_CONFIGURATIONS --type development";
+    "deploy-cluster" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $HOMELAB_CONFIGURATIONS) alucard deploy cluster --flake $HOMELAB_CONFIGURATIONS --type development";
 
-    "build-machine" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $VIRTUAL_MACHINE_CONFIGURATIONS) alucard build machine --flake $VIRTUAL_MACHINE_CONFIGURATIONS";
+    "build-machine" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $VIRTUAL_MACHINE_CONFIGURATIONS) alucard build machine --flake $VIRTUAL_MACHINE_CONFIGURATIONS";
     "start-machine" = "alucard start machine";
 
-    "build-image" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas repository list-overrides $VIRTUAL_MACHINE_CONFIGURATIONS) alucard build image --flake $DISK_IMAGE_CONFIGURATIONS";
+    "build-image" = "ALUCARD_OVERLAYS=\$(dismas package list-overlays) ALUCARD_OVERRIDES=\$(dismas flake list-overrides $VIRTUAL_MACHINE_CONFIGURATIONS) alucard build image --flake $DISK_IMAGE_CONFIGURATIONS";
     "install-image" = "alucard install image";
   };
 }
