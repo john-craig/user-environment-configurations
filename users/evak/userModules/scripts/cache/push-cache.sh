01ad184498b73c73094cea9eb11e2df896e0e599
john-craig <john.e.craig.jr@gmail.com>
2025-05-19 12:58:56 -0400

    #project system administration cache server #task fix up push script for archlinux packages

--- users/evak/userModules/scripts/cache/push-cache.sh
+++ users/evak/userModules/scripts/cache/push-cache.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
 CACHER_ID_PATH="${1:-$HOME/.ssh/cacher}"
 LOCAL_CACHE_LIST=/tmp/pacman-$(date +%s)-cache-list
 LOCAL_CACHE_DIR=/var/cache/pacman/pkg/
 REMOTE_CACHE_DIR=/var/lib/dustman/pacman/hosts/$(hostname)
 
-rsync -av --include='*.pkg.tar.zst' --exclude='*' --out-format='%n' $LOCAL_CACHE_DIR -e 'ssh -i ~/.ssh/cacher' cacher@pxe_server:/srv/cache/pacman/
+rsync -av \
+    --include='*.pkg.tar.zst' \
+    --exclude='*' \
+    --out-format='%n' \
+    --no-perms \
+    --omit-dir-times \
+    $LOCAL_CACHE_DIR \
+    -e 'ssh -i ~/.ssh/cacher' \
+    cacher@pxe_server:/srv/cache/pacman/
 
 ls $LOCAL_CACHE_DIR > $LOCAL_CACHE_LIST
 ssh -i $CACHER_ID_PATH cacher@pxe_server "mkdir -p $REMOTE_CACHE_DIR"
