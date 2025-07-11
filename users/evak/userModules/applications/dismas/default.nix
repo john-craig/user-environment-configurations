65cbd710b944efe9b19d4986bc155d02af97fcd4
john-craig <john.e.craig.jr@gmail.com>
2025-07-11 16:56:09 -0400

    Bump version

--- users/evak/userModules/applications/dismas/default.nix
+++ users/evak/userModules/applications/dismas/default.nix
@@ -7,6 +7,7 @@
 
   home.sessionVariables = {
     DISMAS_VAULT_PATH = "$DOCUMENTS/by_category/vault";
+    DISMAS_DEFAULT_REPO_OWNER = "john-craig";
     DISMAS_PACKAGES_REPO_PATH = "$NIXPKGS_APOCRYPHA";
   };
 }
--- users/evak/userModules/scripts/cache/default.nix
+++ users/evak/userModules/scripts/cache/default.nix
@@ -18,13 +18,13 @@
         echo "Cacher server is unreachable"
         exit 0
       fi
-
+      
       NIX_SSHOPTS="-i ${cacherIdentityFile}" \
       nix copy --substitute-on-destination \
         --no-check-sigs \
         --to https://${cacheURL} \
         ~/.local/state/home-manager/gcroots/current-home
-    
+      
       nix path-info --recursive ~/.local/state/home-manager/gcroots/current-home > $LOCAL_CACHE_LIST_CURR
       
       ssh -i ${cacherIdentityFile} \
@@ -38,11 +38,10 @@
       scp -q -i ${cacherIdentityFile} \
         cacher@${cacheServer}:$REMOTE_CACHE_DIR/prev \
         $LOCAL_CACHE_LIST_PREV
-
+      
       ssh -i ${cacherIdentityFile} cacher@${cacheServer} "nohup dustman 2>&1 | logger --tag 'dustman'"
       
-      NUM_CHANGED=$(diff -y --suppress-common-lines $LOCAL_CACHE_LIST_CURR $LOCAL_CACHE_LIST_PREV | wc -l)
-      echo "Number of changed packages: $NUM_CHANGED"
+      echo Number of changed packages: $(diff -y --suppress-common-lines "$LOCAL_CACHE_LIST_CURR" "$LOCAL_CACHE_LIST_PREV" | wc -l)
       
       rm -f $LOCAL_CACHE_LIST_CURR
       rm -f $LOCAL_CACHE_LIST_PREV
